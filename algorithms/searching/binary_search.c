a0a8df9f78b77b2e0cd6a2a6fcc7787aec7de732
Sharv Naik <82972994+sharvnaik10@users.noreply.github.com>
2025-01-19 15:09:45 +0530

    Update binary_search.c
    memory efficient and more readable

--- algorithms/searching/binary_search.c
+++ algorithms/searching/binary_search.c
@@ -1,43 +1,44 @@
-// C program to implement recursive Binary Search 
-#include <stdio.h> 
-  
-// A recursive binary search function. It returns  
-// location of x in given array arr[l..r] is present,  
-// otherwise -1 
-int binarySearch(int arr[], int l, int r, int x) 
-{ 
-   if (r >= l) 
-   { 
-        int mid = l + (r - l)/2; 
-  
-        // If the element is present at the middle  
-        // itself 
-        if (arr[mid] == x)   
-            return mid; 
-  
-        // If element is smaller than mid, then  
-        // it can only be present in left subarray 
-        if (arr[mid] > x)  
-            return binarySearch(arr, l, mid-1, x); 
-  
-        // Else the element can only be present 
-        // in right subarray 
-        return binarySearch(arr, mid+1, r, x); 
-   } 
-  
-   // We reach here when element is not  
-   // present in array 
-   return -1; 
-} 
-  
-int main(void) //take nothing return something
-{ 
-   int arr[] = {}; //Enter array of elements
-   int n = sizeof(arr)/ sizeof(arr[0]); 
-   int x = 10; 
-   int result = binarySearch(arr, 0, n-1, x); 
-   (result == -1)? printf("Element is not present in array") 
-                 : printf("Element is present at index %d", 
-                                                   result); 
-   return 0; 
-} 
+#include <stdio.h>
+
+// Function to perform binary search
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if the target is at the middle
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // If target is smaller, ignore the right half
+        if (arr[mid] > target) {
+            right = mid - 1;
+        }
+        // If target is larger, ignore the left half
+        else {
+            left = mid + 1;
+        }
+    }
+
+    // Return -1 if target is not found
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
